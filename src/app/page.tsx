27c7acdf750ac0c44ee0640b4680e1ea0583e547
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 08:08:35 -0300

    feat(app): implement api search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,24 @@
-import Image from "next/image";
+'use client';
+
 import { Character } from "@/types/Character";
 import { CharacterRow } from "@/components/CharacterRow";
+import { fetchCharacters } from "@/services/api";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const characters: Character[] = [
-    {
-      name: 'Luke Skywalker',
-      height: 172,
-      spaceships: ['X-wing', 'Imperial shuttle'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Darth Vader',
-      height: 202,
-      spaceships: ['TIE Advanced x1'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Han Solo',
-      height: 180,
-      spaceships: ['Millennium Falcon'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Leia Organa',
-      height: 150,
-      spaceships: ['Tantive IV', 'Millennium Falcon'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Chewbacca',
-      height: 228,
-      spaceships: ['Millennium Falcon'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'R2-D2',
-      height: 96,
-      spaceships: [],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'C-3PO',
-      height: 167,
-      spaceships: [],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Boba Fett',
-      height: 183,
-      spaceships: ['Slave 1'],
-      movies: ['The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Yoda',
-      height: 66,
-      spaceships: [],
-      movies: ['The Empire Strikes Back', 'Return of the Jedi'],
-    },
-    {
-      name: 'Obi-Wan Kenobi',
-      height: 182,
-      spaceships: ['Jedi starfighter'],
-      movies: ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'],
-    },
-  ]
+
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  async function getAllCharacters() {
+    fetchCharacters().then((response) => {
+      console.log(response);
+      setCharacters(response.results);
+    });
+  }
+
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
 
   return (
     <div className="">
