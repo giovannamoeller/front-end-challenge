c90541759e610049e9f289c6b064ffcbe0ae085e
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 08:40:39 -0300

    feat(character): fetch movies from character

--- src/components/CharacterRow.tsx
+++ src/components/CharacterRow.tsx
@@ -1,16 +1,35 @@
 import { Character } from "@/types/Character";
+import { fetchMovie } from "@/services/api";
+import { useEffect, useState } from "react";
 
 interface CharacterRowProps {
   character: Character;
 }
 
 export function CharacterRow({ character }: CharacterRowProps) {
+  const [movies, setMovies] = useState<string[]>([]);
+
+  async function getCharacterMovies() {
+    try {
+      const movies = await Promise.all(
+        character.films.map(url => fetchMovie(url))
+      );
+      setMovies(movies);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    getCharacterMovies();
+  }, []);
+
   return (
     <div className="grid grid-cols-4 gap-4 p-4 border-t border-gray-200 hover:bg-gray-100">
       <h2>{character.name}</h2>
       <p>{character.height} cm</p>
       <p>{character.starships.length}</p>
-      <p className="truncate">{character.films.join(', ')}</p>
+      <p className="truncate">{movies.join(', ')}</p>
     </div>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
-import { APIResponse } from '../types/APIResponse';
+import { APIResponse } from '@/types/APIResponse';
+import { Movie } from '@/types/Movie';
 
 const BASE_URL = 'https://swapi.dev/api';
 
-export async function fetchCharacters(page: number = 1): Promise<APIResponse> {
+async function fetchCharacters(page: number = 1): Promise<APIResponse> {
   try {
     const response = await fetch(`${BASE_URL}/people/?page=${page}`);
     if (!response.ok) {
@@ -14,3 +15,19 @@ export async function fetchCharacters(page: number = 1): Promise<APIResponse> {
     throw error;
   }
 }
+
+async function fetchMovie(url: string): Promise<string> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch movie');
+    }
+    const data = await response.json();
+    return data.title;
+  } catch(error) {
+    console.error('Error fetching movie:', error);
+    throw error;
+  }
+}
+
+export { fetchCharacters, fetchMovie };
