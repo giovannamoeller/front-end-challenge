9a654d1e337c73ecc5bf18b6152da20858813350
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 01:18:26 -0300

    refactor: add new style to main heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-2xl">Personagens dos filmes de Star Wars</h1>
+      <h1 className="text-2xl font-bold text-gray-900 mb-8">Personagens dos filmes de Star Wars</h1>
     </div>
   );
 }
