3674f6a7c607e7bd41494a7f88b886094d38cfd2
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 01:17:11 -0300

    feat: implement service api code, refactoring layout

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import { APIResponse } from '../types/APIResponse';
+
+const BASE_URL = 'https://swapi.dev/api';
+
+export async function getAllCharacters(page: number = 1): Promise<APIResponse> {
+  try {
+    const response = await fetch(`${BASE_URL}/people/?page=${page}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch characters');
+    }
+    return response.json();
+  } catch(error) {
+    console.error('Error fetching characters:', error);
+    throw error;
+  }
+}
