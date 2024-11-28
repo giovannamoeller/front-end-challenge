e3e3e3357be9a9bc4666ab2b5bea668015cfd46b
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 08:50:25 -0300

    style(table header): change table header background color

--- src/components/CharactersTableHeader.tsx
+++ src/components/CharactersTableHeader.tsx
@@ -1,6 +1,6 @@
 export function CharactersTableHeader() {
   return (
-    <div className="grid grid-cols-4 gap-4 p-4 bg-gray-50 font-semibold">
+    <div className="grid grid-cols-4 gap-4 p-4 bg-blue-100 font-semibold">
       <div>Nome</div>
       <div>Altura</div>
       <div>N° de espaçonaves</div>
