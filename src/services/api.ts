68ee92297c7327447291c290435c78ddf28e3580
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-28 08:07:40 -0300

    refator(api): change getAllCharacters function name to fetchCaracters

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { APIResponse } from '../types/APIResponse';
 
 const BASE_URL = 'https://swapi.dev/api';
 
-export async function getAllCharacters(page: number = 1): Promise<APIResponse> {
+export async function fetchCharacters(page: number = 1): Promise<APIResponse> {
   try {
     const response = await fetch(`${BASE_URL}/people/?page=${page}`);
     if (!response.ok) {
